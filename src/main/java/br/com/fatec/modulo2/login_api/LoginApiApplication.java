a1e344a289dc71aafd851df5aa45fe48fcbde66c
bportugal01 <bruno2004sa@gmail.com>
2025-12-01 19:22:27 -0300

    feat(set up environment): Spring boot framework, git ignore, git hooks, api client

---
+++ src/main/java/br/com/fatec/modulo2/login_api/LoginApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.modulo2.login_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApiApplication.class, args);
+	}
+
+}
